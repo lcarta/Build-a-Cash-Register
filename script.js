c9fc61091f6f6dae21b8e943bc0a356bb3fd5882
lcarta/Luca Carta <lucarta@outlook.it>
2025-04-12 17:17:39 +0200

    modified:   script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const changeDue = document.getElementById("change-due");
 
 
 const cashRegisterStatus = ["OPEN", "CLOSED", "INSUFFICIENT_FUNDS"];
-let isInsufficientFunds = false;
+let isStatusOpen = false;
 //Dati imposti dal progetto
 let price = 3.26;
 let cid = [
@@ -40,18 +40,14 @@ let cashValue = 0;
 
 
 //funzione per aggiornamento display
-const updateDisplay = (isInsufficientFunds) => {
+const updateDisplay = () => {
   changeDrawer.innerHTML = "";
   changeDue.innerHTML = ""
   priceValue.innerText = `$ ${price}`;
   cid.forEach((cash) => {
     changeDrawer.innerHTML += `<span><b>${cash[0]}:</b></span> <span class="cid-span-value">$${cash[1]} </span></br> `;  //aggiornamento del resto nel cassetto
   });
-  if (isInsufficientFunds) {
-    changeDue.innerHTML += "Status: INSUFFICIENT_FUNDS";
-    return
-  }
-  changeDue.innerHTML += "Status: OPEN </br>";
+  changeDue.innerHTML += `${isStatusOpen ? "Status: OPEN </br>" : ""}`;
   for (let key in cashAmounts) {
     unitsAmounts.forEach((unit) => {
       if (unit[0] === key) {
@@ -67,7 +63,6 @@ updateDisplay();
 //funzione chiamata dal bottone purchase
 const purchase = () => {
   cashValue = Number(cashInput.value);
-  let changeValue = cashValue - price;
   if (cashValue === price) {
     changeDue.innerHTML = "No change due - customer paid with exact cash"      //controllo dell'input utente
     return;
@@ -76,31 +71,34 @@ const purchase = () => {
     alert("Customer does not have enough money to purchase the item")      //controllo dell'input utente
     return;
   }
+  let changeValue = Number((cashValue - price).toFixed(2));
+
   calculateChange(changeValue);
-  updateDisplay(isInsufficientFunds);
   cashAmounts = {}
 }
 
 
 
 const calculateChange = (changeValue) => {
+  const unitChange2 = cid.filter((_, index) => (unitsAmounts[index][1] / changeValue) <= 1).reduce((acc, el) => (el[1] + acc), 0);
+  console.log(unitChange2)
   while (changeValue > 0) {
     //trova un array dell'unitAmount (formattato dentro checkCid) il quale se diviso per il resto(changeValue) da minore o uguale a 1 e lo salva in unitChange
     const unitChange = checkCid().toReversed().find((cash) => (cash[1] / changeValue) <= 1);
-    if (!unitChange) {
-      console.log("insufficient")
-      isInsufficientFunds = !isInsufficientFunds;
-      console.log(isInsufficientFunds)
+
+    if (unitChange2 < changeValue || !unitChange) {
+      changeDue.innerHTML = "Status: INSUFFICIENT_FUNDS";
       return
     }
+
     //sottrae il valore salvato in unitChange dal resto changeValue approssimandolo alla seconda cifra decimale 
-    changeValue = formatNumber((changeValue - unitChange[1]));
+    isStatusOpen = true;
+    changeValue = Number((changeValue - unitChange[1]).toFixed(2));
 
     //passa il valore trovato alle funzioni 
     updateCid(unitChange[0], unitChange[1]);
     updateCashAmount(unitChange[0]);
-
-
+    updateDisplay();
   }
 }
 
@@ -115,7 +113,7 @@ const updateCashAmount = (cash) => {
 const updateCid = (changeName, changeAmount) => {
   cid.map((value) => {
     if (value[0] === changeName) {
-      value[1] = formatNumber((value[1] - changeAmount));
+      value[1] = Number((value[1] - changeAmount).toFixed(2));
       return
     }
   })
@@ -124,19 +122,25 @@ const updateCid = (changeName, changeAmount) => {
 
 const checkCid = () => unitsAmounts.filter((_, index) => cid[index][1] !== 0);
 
-const changeCashRegisterStatus = (string) => {
-  if (string === "isInsufficient") {
-    return "Status: INSUFFICIENT_FUNDS"
-  } else if (string === "open") {
-    return "Status: OPEN"
-  }
-}
 
+const checkEnoughChange = () => {
+  cashValue = Number(cashInput.value);
+  let changeValue = Number((cashValue - price).toFixed(2));
+
+
+}
 
 const formatNumber = (number) => {
   return number % 1 === 0 ? Number(number) : Number(number.toFixed(2))
 }
 
+
+if (cid.every((value) => value[1] === 0)) {
+  console.log("ok")
+}
+
 purchaseBtn.addEventListener('click', () => {
+  checkEnoughChange();
   (!cashInput.value) ? alert("Insert a customer's cash value") : purchase()
 })
+
